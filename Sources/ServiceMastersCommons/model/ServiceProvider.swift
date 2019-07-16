566d6374e391a668e31d80eb85bb3cb83ebe40a0
Awad Owda <aowda@apple.com>
2019-07-15 21:58:15 -0500

    Fixing service provider model

--- Sources/ServiceMastersCommons/model/ServiceProvider.swift
+++ Sources/ServiceMastersCommons/model/ServiceProvider.swift
@@ -2,6 +2,7 @@
 // Created by Awad Owda on 2019-07-02.
 //
 
+import Foundation
 import CouchDB
 
 public struct ServiceProvider: Document {
@@ -13,12 +14,12 @@ public struct ServiceProvider: Document {
     public let phoneNumber: String
     public let bio: String
     public var department: Department?
-    public var reviews: [Review]
+    public var reviews: Set<Review>
     public var overview: String
-    public var Services: [Service]
+    public var Services: Set<Service>
     public var address: Location
 
-    public init( id: String, name: String, phoneNumber: String, bio: String, department: Department?, reviews: [Review], overview: String, Services: [Service], address: Location) {
+    public init( id: String, name: String, phoneNumber: String, bio: String, department: Department?, reviews: Set<Review>, overview: String, Services: Set<Service>, address: Location) {
         self.serviceProviderId = id
         self.name = name
         self.phoneNumber = phoneNumber
@@ -28,5 +29,18 @@ public struct ServiceProvider: Document {
         self.overview = overview
         self.Services = Services
         self.address = address
+    }
+    
+    public static func ==(lhs: ServiceProvider, rhs: ServiceProvider) -> Bool {
+        if lhs._id == rhs._id && lhs._rev == rhs._rev && lhs.serviceProviderId == rhs.serviceProviderId {
+            return true
+        }
+        return false
+    }
+}
+
+extension User: Hashable {
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(serviceProviderId)
     }
 }
